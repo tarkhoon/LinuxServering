6cc7241ce40b66e655bdd6677d56ce84b7d6b17b
tarkhoon <49470075+tarkhoon@users.noreply.github.com>
2025-02-25 16:55:06 +0900

    test

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ app = Flask(__name__)
 @app.route("/")
 def index_page():
    message = 'Добро пожаловать! v2'
-   return render_template('index.html', message=message)
+   url ='https://banner2.cleanpng.com/20190612/qbz/kisspng-github-pages-computer-icons-computer-software-github-logo-media-social-icon-5d007ece2bd8d3.7097023515603135501796.jpg'
+   return render_template('index.html', message=message, url=url)
